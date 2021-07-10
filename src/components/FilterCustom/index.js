a24f7bfd3b753e11c7afd074962768ef1f453f06
ekaterina-popa <ecaterina.popa.2021@gmail.com>
2021-07-10 09:49:52 +0300

    add movie trailer modal

---
+++ src/components/FilterCustom/index.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./index.css";
+
+const FilterCustom = () => {
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+        }}
+      >
+        <div>
+          {" "}
+          <Link to="/admin/movies/movie-add">
+            <button
+              className="btn btn-secondary wrn-btn"
+              style={{ background: "green" }}
+            >
+              Add Movie
+            </button>
+          </Link>
+        </div>
+        <form>
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="row">
+                <div className="col-lg-3 col-md-3 col-sm-12 p-0">
+                  <input
+                    type="text"
+                    className="form-control search-slt"
+                    placeholder="Genre"
+                  />
+                </div>
+                <div className="col-lg-3 col-md-3 col-sm-12 p-0">
+                  <input
+                    type="text"
+                    className="form-control search-slt"
+                    placeholder="Release Date"
+                  />
+                </div>
+                <div className="col-lg-3 col-md-3 col-sm-12 p-0">
+                  <select
+                    className="form-control search-slt"
+                    id="exampleFormControlSelect1"
+                  >
+                    <option>Age Restrict</option>
+                    <option>Example one</option>
+                    <option>Example one</option>
+                    <option>Example one</option>
+                    <option>Example one</option>
+                    <option>Example one</option>
+                    <option>Example one</option>
+                  </select>
+                </div>
+                <div className="col-lg-3 col-md-3 col-sm-12 p-0">
+                  <button className="btn btn-secondary wrn-btn">Filter</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default FilterCustom;
--- src/pages/Movie/SingleMovie.js
+++ src/pages/Movie/SingleMovie.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useHistory } from "react-router";
 import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
 import { Modal, Button } from "react-bootstrap";
 import { deleteMovie } from "../../actions/Movie";
+import CardCustom from "../../components/CardCustom";
 import axios from "axios";
-import "./style.css";
 
 const SingleMovie = () => {
   const params = useParams();
@@ -37,58 +36,11 @@ const SingleMovie = () => {
     });
   };
 
-  console.log("single-movie: ", movie.release_date);
+  //console.log("single-movie: ", movie.release_date);
   return (
     <>
       <div className="container">
-        <div className="main_card">
-          <div className="card_left">
-            <div className="card_datails">
-              <h4>{movie.title}</h4>
-              <p>Original title: {movie.original_title}</p>
-              <div className="card_cat">
-                <p className="PG">{movie.age_restrict}</p>
-                <p className="year">2021</p>
-                <p className="genre">{movie.genre}</p>
-                <p className="time">{movie.duration}</p>
-              </div>
-              <p className="disc">{movie.description}</p>
-              <p className="crew">
-                Director: {movie.director}
-                <br></br>
-                Stars: {movie.actors}
-              </p>
-              <div className="social-btn row">
-                <div className="col-md-4">
-                  <button className="crd-button">
-                    <i className="fas fa-play "></i> SEE TRAILER
-                  </button>
-                </div>
-                <div className="col-md-3">
-                  <button className="crd-button">
-                    <i className="fas fa-star "></i>
-                    <span> {movie.rating}</span>
-                  </button>
-                </div>
-                <div className="col-md-2">
-                  <Link to={`/admin/movies/movie-edit/${movie._id}`}>
-                    <button className="btn btn-primary">Edit</button>
-                  </Link>
-                </div>
-                <div className="col-md-3">
-                  <button className="btn btn-danger" onClick={handleShow}>
-                    Delete
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="card_right">
-            <div className="img_container">
-              <img src={movie.image_url} alt="" />
-            </div>
-          </div>
-        </div>
+        <CardCustom movie={movie} handleShow={handleShow} />
       </div>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
