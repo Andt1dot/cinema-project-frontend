ce3ebc8e1e218eba3a8541b9de1dc4fccd2c50cc
Andt1dot <marin.cebotari@112MD.local>
2021-08-21 00:06:14 +0300

    addRequestInBoxMessage

--- src/actions/Auth/ResetPassword.js
+++ src/actions/Auth/ResetPassword.js
@@ -1,18 +1,12 @@
 import axios from "axios";
-// export const RESET_PASSWORD_REQUEST = "RESET_PASSWORD_REQUEST";
-// export const RESET_PASSWORD_SUCCESS = "RESET_PASSWORD_SUCCESS";
-// export const RESET_PASSWORD_FAILURE = "RESET_PASSWORD_FAILURE";
 
 export const requestResetPassword = async (email) => {
   return await axios
-    .post(process.env.REACT_APP_API_URL+'/auth/request/reset-password', {
+    .post(process.env.REACT_APP_API_URL + "/auth/request/reset-password", {
       email,
     })
     .then((user) => {
       return user.data;
-
-      //localStorage.setItem("token", user.data.token);
-      // localStorage.setItem("isAdmin", user.data.role);
     })
     .catch((error) => {
       return JSON.parse(error.request.response);
@@ -20,9 +14,11 @@ export const requestResetPassword = async (email) => {
 };
 
 export const resetPassword = async (user_id, new_password) => {
-  return await axios
-    .patch(process.env.REACT_APP_API_URL+'/auth/reset-password/', {
+  return await axios.patch(
+    process.env.REACT_APP_API_URL + "/auth/reset-password/",
+    {
       user_id,
       new_password,
-    });
+    }
+  );
 };
--- src/components/EmailForm/index.js
+++ src/components/EmailForm/index.js
@@ -1,79 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 
-const EmailForm = () => {
+const EmailForm = ({ onSendMessage }) => {
+  const [inputState, setInputState] = useState({
+    email: "",
+    title: "",
+    content: "",
+  });
+
+  const handleInputChange = (e) => {
+    setInputState({ ...inputState, [e.target.name]: e.target.value });
+  };
+
+  const handleClickMessage = (e) => {
+    onSendMessage(inputState.email, inputState.content, inputState.title);
+  };
+
   return (
-    <form action="mail.php" method="post">
-      <div class="card">
-        <div class="card-header p-0 m-0">
-          <div class="bg-dark text-white text-center py-2 rounded-1">
-            <h3>
-              <i class="fa fa-envelope"></i> Mesagerie electronică
-            </h3>
-            <p class="m-0">Cu ce vă putem ajuta ? </p>
-          </div>
+    <div className="card">
+      <div className="card-header p-0 m-0">
+        <div className="bg-dark text-white text-center py-2 rounded-1">
+          <h3>
+            <i className="fa fa-envelope"></i> Mesagerie electronică
+          </h3>
+          <p className="m-0">Cu ce vă putem ajuta ? </p>
         </div>
-        <div class="card-body p-3">
-          <div class="form-group">
-            <div class="input-group mb-2">
-              <div class="input-group-prepend">
-                <div class="input-group-text">
-                  <i class="fa fa-user text-dark"></i>
-                </div>
+      </div>
+      <div className="card-body p-3">
+        <div className="form-group">
+          <div className="input-group mb-2">
+            <div className="input-group-prepend">
+              <div className="input-group-text">
+                <i className="fa fa-user text-dark"></i>
               </div>
-              <input
-                type="text"
-                class="form-control"
-                id="nombre"
-                name="nombre"
-                placeholder="Numele"
-                required
-              />
             </div>
-          </div>
-          <div class="form-group">
-            <div class="input-group mb-2">
-              <div class="input-group-prepend">
-                <div class="input-group-text">
-                  <i class="fa fa-envelope text-dark"></i>
-                </div>
-              </div>
-              <input
-                type="email"
-                class="form-control"
-                id="nombre"
-                name="email"
-                placeholder="Adresa electronică"
-                required
-              />
-            </div>
-          </div>
-
-          <div class="form-group">
-            <div class="input-group mb-2">
-              <div class="input-group-prepend">
-                <div class="input-group-text">
-                  <i class="fa fa-comment text-dark"></i>
-                </div>
-              </div>
-              <textarea
-                class="form-control"
-                placeholder="Textul mesajului"
-                required
-              ></textarea>
-            </div>
-          </div>
-
-          <div class="text-center">
             <input
-              type="submit"
-              value="Expediază"
-              class="btn btn-warning  btn-block rounded-10"
+              type="text"
+              className="form-control"
+              onChange={handleInputChange}
+              id="title"
+              name="title"
+              value={inputState.title}
+              placeholder="Subiectul"
+              required
             />
           </div>
         </div>
+        <div className="form-group">
+          <div className="input-group mb-2">
+            <div className="input-group-prepend">
+              <div className="input-group-text">
+                <i className="fa fa-envelope text-dark"></i>
+              </div>
+            </div>
+            <input
+              type="email"
+              className="form-control"
+              onChange={handleInputChange}
+              id="email"
+              name="email"
+              value={inputState.email}
+              placeholder="Adresa electronică"
+              required
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="input-group mb-2">
+            <div className="input-group-prepend">
+              <div className="input-group-text">
+                <i className="fa fa-comment text-dark"></i>
+              </div>
+            </div>
+            <textarea
+              className="form-control"
+              name="content"
+              value={inputState.content}
+              placeholder="  Textul mesajului"
+              required
+              onChange={handleInputChange}
+            ></textarea>
+          </div>
+        </div>
+
+        <div className="text-center">
+          <input
+            type="submit"
+            value="Expediază"
+            className="btn btn-warning  btn-block rounded-10"
+            onClick={handleClickMessage}
+          />
+        </div>
       </div>
-    </form>
+    </div>
   );
 };
 
